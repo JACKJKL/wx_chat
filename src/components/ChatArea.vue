88eee097e3e16f41b76a8c33ee5be0959cc95d69
唐晓斌/技术部 <1160224892@qq.com>
2018-07-17 11:07:17 +0800

    add pug

--- src/components/ChatArea.vue
+++ src/components/ChatArea.vue
@@ -3,38 +3,30 @@
 @import "../assets/css/chat_area";
 </style>
 
-<template>
-  <div class="chat-area">
-    <div class="box-hd">
-      <div class="title">
-        南源
-      </div>
-    </div>
-    <div class="chat-bd">
-      <div class="msg-item clearfix" v-for="x in '123'">
-        今天天气如何？
-      </div>
-      <div class="msg-item clearfix">
-        还好吧
-      </div>
-    </div>
-    <div class="m-ft">
-      <ul class="tool-tab">
-        <li><i class="iconfont icon-face"></i></li>
-        <li><i class="iconfont icon-cut"></i></li>
-        <li><i class="iconfont icon-file"></i></li>
-      </ul>
-      <div class="content">
-        <div class="input" contenteditable="true">
-          <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3062734635,4183904616&fm=96">
-        </div> 
-      </div>
-      <div class="action">
-        <span class="notice">按ctrl+enter发送</span>
-        <a href="javascript:void(0)" class="u-send">发送</a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  div.chat-area
+    div.box-hd
+      div.title 南源
+
+    div.chat-bd
+      div.msg-item.clearfix(v-for="x in '123'")
+        div 今天天气如何
+
+    div.m-ft
+      ul.tool-tab
+        li
+          i.iconfont.icon-face
+        li
+          i.iconfont.icon-cut
+        li
+          i.iconfont.icon-file
+      div.content
+        div.input(contenteditable="true")
+          img(src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3062734635,4183904616&fm=96")
+
+      div.action
+        span.notice 按ctrl+enter发送
+        a.u-send(href="javascript:void(0)") 发送
 </template>
 
 <script>
